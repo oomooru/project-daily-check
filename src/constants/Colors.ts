af584d3e9ad2a58481d53366691bf513d452375d
oomooru <oomooru.dev@gmail.com>
2025-04-28 20:57:20 +0900

    I missed '#' :(

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -6,8 +6,8 @@ interface ColorPalette {
 }
 
 export const colors: ColorPalette = {
-  primary: '7AE2CF',
-  secondary: '077A7D',
-  background: '06202B',
-  text: 'F5EEDD'
+  primary: '#7AE2CF',
+  secondary: '#077A7D',
+  background: '#06202B',
+  text: '#F5EEDD'
 } as const;
