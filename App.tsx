698fa5bdabb7f5a05400118979b2a36b486cc584
oomooru <oomooru.dev@gmail.com>
2025-04-28 15:37:38 +0900

    Add SafeAreaProvider at App.tsx and Add style for SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { MainPage } from './src/components/pages/MainPage';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <MainPage />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.container}>
+        <MainPage />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
