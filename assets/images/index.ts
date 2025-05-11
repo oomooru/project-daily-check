dabc502840c7a0832f2c0d443dd137c139afa241
oomooru <oomooru.dev@gmail.com>
2025-05-11 15:23:15 +0900

    Add new svg icon.

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,2 +1,6 @@
 export {default as Logo} from './logo.svg';
 export {default as IconClock} from './iconClock.svg';
+export {default as Calendar} from './calendar.svg';
+export {default as CheckSquare} from './checkSquare.svg';
+export {default as Edit} from './edit.svg';
+export {default as Plus} from './plus.svg';
