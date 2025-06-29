0df3deae14e45d37884c1b9c743f234cc07414f2
oomooru <oomooru.dev@gmail.com>
2025-06-29 18:18:11 +0900

    Fix text alignment

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -32,7 +32,7 @@ export const Text: React.FC<TextProps> = ({
         { 
           fontFamily: language === "ja" ? "NotoSansKR" : "NotoSansJP",
           includeFontPadding: false,
-          textAlignVertical: 'center',
+          textAlignVertical: 'top',
         },
       ]}
       onLayout={onLayout}
